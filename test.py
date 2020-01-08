c0022a04ec2a136326d92b589ed1f7300c870369
HorizonFTT <HorizonFTT@outlook.com>
2020-01-08 20:51:11 +0800

    update

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 # https://www.codewars.com/kata/526571aae218b8ee490006f4/train/python
+import operator
+
+
 def countBits(n):
     binary = bin(n)
     return binary.count('1')
@@ -139,7 +142,6 @@ def expanded_form(num):
 # minus = lambda x: lambda y: y - x
 # times = lambda x: lambda y: y * x
 # divided_by = lambda x: lambda y: y / x
-import operator
 
 
 def zero(): return t[0](0, t[1]) if t else 0
@@ -190,14 +192,93 @@ def divided_by(n):
 
 # https://www.codewars.com/kata/format-a-string-of-names-like-bart-lisa-and-maggie/train/python
 def namelist(names):
-    str=', '.join(d['name'] for d in names)
-    str=str[::-1].replace(',','& ',1)
+    str = ', '.join(d['name'] for d in names)
+    str = str[::-1].replace(',', '& ', 1)
     return str[::-1]
 
 
 # https://www.codewars.com/kata/dubstep/train/python
 def song_decoder(song):
-    list=song.split('WUB')
+    list = song.split('WUB')
     return ' '.join(s for s in list if s != '')
 
-    eval
+
+# https://www.codewars.com/kata/5839edaa6754d6fec10000a2/train/python
+def find_missing_letter(chars):
+    # next(chr(ord(c[i])+1) for i in range(len(c)-1) if ord(c[i])+1 != ord(c[i+1]))
+    ords = [ord(c) for c in chars]
+    for i in range(1, len(ords)):
+        if ords[0] + i != ords[i]:
+            return chr(ords[i] - 1)
+
+
+# https://www.codewars.com/kata/extract-the-domain-name-from-a-url-1/train/python
+def domain_name(url):
+    # return url.split("//")[-1].split("www.")[-1].split(".")[0]
+    # return re.search('(https?://)?(www\d?\.)?(?P<name>[\w-]+)\.', url).group('name')
+    list = url.split('//')
+    if len(list) > 1:
+        url = list[1]
+    else:
+        url = list[0]
+    url.split('/')[0]
+    list = url.split('www.')
+    if len(list) > 1:
+        url = list[1]
+    else:
+        url = list[0]
+    return url.split('.')[0]
+
+
+# https://www.codewars.com/kata/valid-parentheses/train/python
+def valid_parentheses(string):
+    # cnt = 0
+    # for char in string:
+    #     if char == '(': cnt += 1
+    #     if char == ')': cnt -= 1
+    #     if cnt < 0: return False
+    # return True if cnt == 0 else False
+    stack = []
+    for c in string:
+        if c == '(':
+            stack.append(c)
+        if c == ')':
+            if len(stack) == 0 or stack.pop() != '(':
+                return False
+    if len(stack) != 0:
+        return False
+    return True
+
+
+# https://www.codewars.com/kata/sudoku-solution-validator/train/python
+def validSolution(board):
+    flags = []
+    for i in range(9):
+        flags.append(False)
+    for i in range(3):
+        for j in range(3):
+            for x in range(3):
+                for y in range(3):
+                    value = board[3 * i + x][3 * j + y]
+                    if value == 0:
+                        return False
+                    flags[value - 1] = True
+            for k in range(9):
+                if flags[k] == False:
+                    return False
+                flags[k] = False
+    for i in range(9):
+        for j in range(9):
+            flags[board[i][j] - 1] = True
+        for k in range(9):
+            if flags[k] == False:
+                return False
+            flags[k] = False
+    for i in range(9):
+        for j in range(9):
+            flags[board[j][i] - 1] = True
+        for k in range(9):
+            if flags[k] == False:
+                return False
+            flags[k] = False
+    return True
